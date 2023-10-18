4fd8d0a1752b04cb49750d93750e75349c51e1da
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2023-10-18 13:26:15 +0300

    Update index.js

--- src/pages/publications/index.js
+++ src/pages/publications/index.js
@@ -11,24 +11,24 @@ function Publications() {
 	const name = "S. Chatzopoulos"; // TODO
 
 	const makeBold = (input) => {
-       var re = new RegExp(name, 'g');
-       return (
-           input.replace(re, '<b className="colorSuccess">' + name + '</b>')
-       );
+	       var re = new RegExp(name, 'g');
+	       return (
+	           input.replace(re, '<b className="colorSuccess">' + name + '</b>')
+	       );
 	}
 
-	console.warn(_.groupBy (publications, 'year') );
+	// console.warn(_.groupBy (publications, 'year') );
 	return (
 		<div id="publications">
 	        <div className={projectStyles.projectPageHeader}>
 	          <h2 className="underlineColorSuccess">My publications</h2>
 	        </div>
 
-	        <div className={styles.section}>
+	        <div className={styles.section} style={{ textAlign: 'center' }}>
 	        {
 	        	publications.map( (paper, i) => {
 	        		return (
-	        			<div key={ `paper-${i}` } className="col col--6 col--offset-4 margin-bottom--lg">
+	        			<div key={ `paper-${i}` } className="col col--6 col--offset-3 margin-bottom--lg">
 		                    <h3 className={styles.title}>{ paper.title }</h3>
 		                    <div id="venue-year">
 		                    	<em>{ paper.venue } &middot; { paper.year }</em>
